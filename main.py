7dbbc234ccad3ff7874af58324b0477c938cfe90
Zain Khan <zain.9496@gmail.com>
2023-07-20 03:12:39 +0500

    Added jacobian inverse calculation for force and torque conversion

--- main.py
+++ main.py
@@ -33,18 +33,24 @@ z = z_base
 pos = 0
 
 R = get_rot_mat(x = 0, y = -np.pi/2, z = 0)
+robot.MoveTo(x, y, z)
 for i in range(10000):
-	# robot.MoveTo(x, y, z)
-
 	state = robot.GetCurrentState()
 	J_robotjointbase, J_robotframe = robot.GetJabobian(state.GetPosition())
 
-	q_dot = state.GetTorque()[:, np.newaxis]
+	if np.linalg.det(J_robotjointbase) != 0:
+		J_robotjointbase_inv = np.linalg.inv(J_robotjointbase)
 
-	v_dot = J_robotjointbase@q_dot
-	v_dot = robot.R_robotframe_robotjointbase.T@v_dot
+		torque = state.GetTorque()[:, np.newaxis]
+		force = J_robotjointbase@torque
+		torque_calculated = J_robotjointbase_inv@force
 
-	print(q_dot.T)
+		torque_str = f"Torque - ({round(torque[0, 0], 2)}, {round(torque[1, 0], 2)}, {round(torque[2, 0], 2)})"
+		force_str = f"Force - ({round(force[0, 0], 2)}, {round(force[1, 0], 2)}, {round(force[2, 0], 2)})"
+		torque_calculated_str = f"Force - ({round(torque_calculated[0, 0], 2)}, {round(torque_calculated[1, 0], 2)}, {round(torque_calculated[2, 0], 2)})"
+
+		print(f"{torque_str}, {force_str}, {torque_calculated_str}")
+
 
 	p.stepSimulation()
 	time.sleep(1./240.)
